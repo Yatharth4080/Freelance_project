cf43ee0d7cd36e9df8751d5dcf33bd018dd9828b
Yatharth4080 <iampuneet982@gmail.com>
2024-05-12 12:57:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev",
+  // },
+});
+
+
+var box = document.querySelector(".box img")
+var shopnow = document.querySelector(".shopnow");
+
+box.addEventListener("mousemove", function (dets) {
+  setTimeout(() => {
+    shopnow.style.left = dets.x + "px"
+    shopnow.style.top = dets.y + "px"
+    shopnow.style.opacity = 1
+
+
+  }, 0);
+
+
+
+  box.addEventListener("mouseleave", function () {
+    shopnow.style.opacity = 0
+  })
+
+})
+
+
+
+
+
+
